6bf066a1ef35713d50fe37d28a4feb7112f18189
cwiebe38 <cody.wiebe@tufts.edu>
2021-06-25 09:38:46 -0700

    Functionized password code.

--- securePassword.py
+++ securePassword.py
@@ -5,22 +5,15 @@
 import string
 import random
 
-# Getting the users name
-Name = input("What is your name: ")
-
-# Do they want a password generated?
-password = input("Would you like a password? ")
-
-if password == "yes":
-
-    #basic password generator
+#basic password generator
+def basicPassword() :
     length = input("How long do you want your password to be? ")
     size = int(length)
     print(f'Ok {Name} your new password is: ')
     print(''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(size)))
-elif password == "advanced":
 
-    #password generator with specific numbers of each of the character types
+#password generator with specific numbers of each of the character types
+def advancedPassword() :
     length = input("How long do you want your password to be? ")
     size = int(length)
     letters = input("How many letters do you want in your password? ")
@@ -31,14 +24,24 @@ elif password == "advanced":
     nums = int(numbers)
 
     if lets + nums > size:
-        print('That is too many numbers and letters')
+        print('That is too many numbers and letters.')
         exit()
     chars = size - lets - nums
 
     print(f'You want a password of {size} length with {lets} letters, {nums} numbers and {chars} special characters')
     print(f'I don\'t feel like coding that so you\'re getting a password length {size}: ')
     print(''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(size)))
-else:
-    print('Ok bye.')
+
+# Getting the users name
+Name = input("What is your name: ")
+
+# Do they want a password generated?
+password = input("Would you like a password? ")
+
+if password == "yes":
+    basicPassword()
+elif password == "advanced":
+    advancedPassword()   
 print('Ok bye.')
 exit()
+
